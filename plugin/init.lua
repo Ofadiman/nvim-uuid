5688e2dec50e5b91466462d191b953b4c94c9376
ofadiman <ofadiman@gmail.com>
2024-01-08 17:09:11 +0100

    chore: delete plugin/ directory

--- plugin/init.lua
+++
@@ -1,7 +0,0 @@
-require("nvim-uuid")
-
--- This file will be executed automatically when the plugin is loaded, so all global functions declared in this file will be available to lua.
--- For example, `vim.api.nvim_set_keymap("n", "<leader>ut", "<cmd>lua GlobalFunction()<CR>", { noremap = true, silent = true })` will allow to execute global function.
-function GlobalFunction()
-  print("debugging...")
-end
