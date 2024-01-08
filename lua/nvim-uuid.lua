6b0f18d8fef6d76519b1dd789951fca4face3be0
ofadiman <ofadiman@gmail.com>
2024-01-08 15:39:23 +0100

    feature: generate uuid using uuidgen package

--- lua/nvim-uuid.lua
+++ lua/nvim-uuid.lua
@@ -1,7 +1,23 @@
 local M = {}
 
 function M.uuid()
-  print("uuid function called")
+  local command = "uuidgen"
+  local uuidgenOutput = ""
+  local handle = io.popen(command, "r")
+  if handle then
+    uuidgenOutput = handle:read("*a")
+    handle:close()
+  else
+    print("command \"" .. command .. "\" not available.")
+    return
+  end
+
+  -- Trim whitespaces and new lines from generated uuid string
+  local uuid = uuidgenOutput:gsub("^[%s\n]*(.-)[%s\n]*$", "%1")
+  local _, col = unpack(vim.api.nvim_win_get_cursor(0))
+  local line = vim.api.nvim_get_current_line()
+  local new_line = line:sub(1, col) .. uuid .. line:sub(col + 1)
+  vim.api.nvim_set_current_line(new_line)
 end
 
 -- The `setup` function is a convention in the neovim plugin environment that allows to configure a plugin.
